80257466eae46724752c89e7eb54c32ee4e800b8
Pedro Henrique De Mattos Salco <pedroh.mattos@yahoo.com.br>
2026-05-01 22:52:20 +0000

    Praticando JS

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 function Home() {
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Mãe esse é meu site, estou aprendendo :D';
-
-    const br = document.createElement('br');
-
-    const texto = document.createTextNode('Uhuuuuuuuuuuuuuul');
-
-    document.body.append(h1, br, texto);
-    return document.body.append(h1, br, texto)
+    return <h1>Mãe esse é meu site, estou aprendendo :D</h1>>
 }
 
 export default Home;
