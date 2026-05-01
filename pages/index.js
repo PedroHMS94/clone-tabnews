dcbf951f8616b50418797679ffc9aae85a6dd852
Pedro Henrique De Mattos Salco <pedroh.mattos@yahoo.com.br>
2026-05-01 22:45:16 +0000

    Praticando JS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Mãe esse é meu site, estou aprendendo :D</h1>
+    var retorno = '<h1>Mãe esse é meu site, estou aprendendo :D</h1>'
+    retorno = retorno + '<br>'
+    retorno = retorno + 'Uhuuuuuuuuuuuuuul'
+    return retorno
 }
 
 export default Home;
